2327fa0b3f5775c51ba9eb20792974bec74145e2
Fernando Rhenals <rhenalsf@gmail.com>
2023-08-24 10:26:51 -0500

    Login sencillo MODELO MVC

--- index.php
+++ index.php
@@ -1,31 +1,17 @@
 <?php
-require_once(__DIR__ . "/db/db.php");
-require_once(__DIR__ . "/controllers/personas_controller.php");
 
-$controller = new personas_controller();
+require_once 'db/db.php';
+require_once 'controllers/AuthController.php';
 
-if(isset($_POST['guardar_persona'])){
-    $controller->guardar();
+$db = Conectar::conexion();
+$authController = new AuthController($db);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $authController->login($username, $password);
 }
 
-$controller->mostrarLista();
-?>
+include 'views/login.php';
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>GUARDAR PERSONA</h1>
-    <form method="post" action="">
-        <label>Nombres</label>
-        <input type="text" name="nombre" required><br>
-        <label>Identificacion: </label>
-        <input type="text" name="identificacion" required><br>
-        <button type="submit" name="guardar_persona"> GUARDAR</button>
-    </form>
-</body>
-</html>
+?>
