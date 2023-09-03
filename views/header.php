7267dfd3a42abb1ba536f20d1fc72714060bcb94
Fernando Rhenals <rhenalsf@gmail.com>
2023-09-03 12:36:36 -0500

    Se agrega barra de menu, bootstrap y jquery

--- views/header.php
+++ views/header.php
@@ -1,19 +1,48 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CONTROL REPARACIÓN DE COMPUTADORES</title>
+    <title>REPARACION DE COMPUTADORAS</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
     <header>
-        <h1>Encabezado de la pagina</h1>
+        <!-- Encabezado común a todas las páginas -->
+        <h1>Encabezado de la página</h1>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Tu Compañía</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="home.php">Inicio <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Agregar
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Agregar orden</a>
+                    <a class="dropdown-item" href="#">Agregar cliente</a>
+                    <a class="dropdown-item" href="#">Agregar Equipo</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Ver
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Ver ordenes</a>
+                    <a class="dropdown-item" href="#">Ver computadores</a>
+                    <a class="dropdown-item" href="#">Ver equipos</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contacto</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
     </header>
-   <nav>
-    <ul>
-        <li><a href="../index.php">Inicio</a></li>
-        <li><a href="pagina2.html">Acerca de</a></li>
-        <li><a href="pagina3.html">Pagina 3</a></li>
-    </ul>
-   </nav>
-   </header>
