38c1694801489db9a5fd45bf0a867f376010b79e
Fernando Rhenals <rhenalsf@gmail.com>
2023-09-04 12:29:30 -0500

    se agrega vista Agregar Orden

--- views/header.php
+++ views/header.php
@@ -8,23 +8,22 @@
 <body>
     <header>
         <!-- Encabezado común a todas las páginas -->
-        <h1>Encabezado de la página</h1>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Tu Compañía</a>
+    <a class="navbar-brand" href="../views/homeView.php">NANDO_REPARACIONES</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="home.php">Inicio <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="../views/homeView.php">Inicio <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Agregar
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Agregar orden</a>
+                    <a class="dropdown-item" href="../views/agregarOrdenView.php">Agregar orden</a>
                     <a class="dropdown-item" href="#">Agregar cliente</a>
                     <a class="dropdown-item" href="#">Agregar Equipo</a>
                 </div>
@@ -53,6 +52,7 @@
     <ul class="navbar-nav ml-auto"> <!-- Coloca elementos a la derecha -->
         <li class="nav-item">
             <a class="nav-link" href="../logout.php">Cerrar Sesión</a>
+
         </li>
     </ul>
 </div>
