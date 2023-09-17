56783c60204437cbdabbf0708dd08888d9090172
Fernando Rhenals <rhenalsf@gmail.com>
2023-09-17 10:41:26 -0500

    Ajuste en Session

--- config/config.php
+++ config/config.php
@@ -4,4 +4,12 @@ define("BASE_URL", "http://localhost/nando_reparaciones/");
 if (filter_var(BASE_URL, FILTER_VALIDATE_URL) === false) {
     die("La URL base no es válida.");
 }
+function redirigirSiNoHaySesion() {
+    session_start();
+    if (!isset($_SESSION['user_id'])) {
+        header('Location: ' . BASE_URL . 'views/loginView.php');
+        exit();
+    }
+}
+
 ?>
