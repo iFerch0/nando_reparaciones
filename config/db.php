c0e89438b4dffe618f3a15c82d40b44750a63b60
Fernando Rhenals <rhenalsf@gmail.com>
2023-09-21 10:24:16 -0500

    prueba db

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
-define("DB_SERVER", "127.0.0.1");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
-define("DB_DATABASE", "nando_reparaciones");
+define("DB_SERVER", "ebh2y8tqym512wqs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
+define("DB_USERNAME", "ordrhroqn9a7t0lw");
+define("DB_PASSWORD", "lyhaokg70m3u7f5z");
+define("DB_DATABASE", "ng0ks1scgajuj9qf");
 
 try {
     $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
